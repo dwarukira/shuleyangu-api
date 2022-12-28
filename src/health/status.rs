2b16467d8dc2fb5da234bfc32d030d24771c0b08
Duncan Warukira <dwarukira@gmail.com>
2022-12-28 14:39:40 +0300

    new db env

--- src/health/status.rs
+++ src/health/status.rs
@@ -1,10 +1,3 @@
-use actix_web::{
-    web::{self, Data},
-    HttpResponse,
-};
-
-use crate::database::PostgresPool;
-
 // pub async fn test_database(
 //    pool: Data<PostgresPool>,
 // ) -> Result<HttpResponse, Error> {
